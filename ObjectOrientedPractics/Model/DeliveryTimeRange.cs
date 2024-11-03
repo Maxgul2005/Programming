b954ffb9e67e4fbe9ca7294646e7e14f372981cb
Maxgul2005 <msgul2005@gmail.com>
2024-11-03 14:57:46 +0300

    Добавил класс PrioretyOrder, который наследуется от класса Order

---
+++ ObjectOrientedPractics/Model/DeliveryTimeRange.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// Перечисление для диапазонов времени доставки.
+/// </summary>
+public enum DeliveryTimeRange
+{
+    Morning9To11,
+    LateMorning11To13,
+    EarlyAfternoon13To15,
+    Afternoon15To17,
+    Evening17To19,
+    LateEvening19To21
+
+}
+    
+
---
+++ ObjectOrientedPractics/Model/PriorityOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PriorityOrder: Order
+{
+    // Дополнительное поле для хранения времени доставки
+    public DateTime DeliveryTime { get; set; }
+    public DeliveryTimeRange DeliveryTimeRange { get; set; } 
+    public PriorityOrder(OrderStatus status, string dateOfCreation, List<Item> items, DateTime deliveryTime, DeliveryTimeRange deliveryTimeRange)
+       : base(status, dateOfCreation, items)
+    {
+        DeliveryTime = deliveryTime;
+        DeliveryTimeRange = DeliveryTimeRange;
+    }
+}
+
